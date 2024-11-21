425bccf4e8d97b38bbfb3c14f51e6457063198bb
Pranjal Snehi <pranjal.snehi@gmail.com>
2024-11-21 18:44:52 +0530

    Minor changes

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 // import getReactNativePersistence from "@react-native-firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -8,12 +7,7 @@ import { router } from "expo-router";
 import { signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import Secrets from './Secrets.json';
-import Config from "react-native-config";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: Secrets.API_KEY,
   authDomain: Secrets.AUTH_DOMAIN,
